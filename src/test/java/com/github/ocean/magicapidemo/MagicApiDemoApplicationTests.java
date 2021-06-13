4736a780e2e19437c4ed97cb35ba1fc25b4770f0
ocean_wll <ocean_wll@163.com>
2021-06-13 13:05:41 +0800

    初始化提交

---
+++ src/test/java/com/github/ocean/magicapidemo/MagicApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ocean.magicapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
